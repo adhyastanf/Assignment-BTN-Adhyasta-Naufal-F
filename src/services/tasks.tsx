786028d0be4603d2c53a79f62572a2109658361f
Adhyasta Naufal Faadhilah <adhyastanf@Adhyastas-MacBook-Air.local>
2026-06-10 21:41:33 +0700

    tes env

--- src/services/tasks.tsx
+++ src/services/tasks.tsx
@@ -2,9 +2,11 @@
 const API_URL =
   process.env.NODE_ENV === 'production'
     ? `https://${process.env.VERCEL_URL}`
-    : process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+    : process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
     
 export async function fetchTasks() {
+  console.log('VERCEL_URL:', process.env.VERCEL_URL);
+  console.log('API_URL:', API_URL);
   const res = await fetch(`${API_URL}/api/tasks`);
 
   return res.json();
