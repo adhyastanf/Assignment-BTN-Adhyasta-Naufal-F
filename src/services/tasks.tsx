b63ca878e5325200b6701db86ca511da2349c828
Adhyasta Naufal Faadhilah <adhyastanf@Adhyastas-MacBook-Air.local>
2026-06-10 21:46:01 +0700

    set env url in service

--- src/services/tasks.tsx
+++ src/services/tasks.tsx
@@ -1,12 +1,6 @@
+const API_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
 
-const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? `https://${process.env.VERCEL_URL}`
-    : process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
-    
 export async function fetchTasks() {
-  console.log('VERCEL_URL:', process.env.VERCEL_URL);
-  console.log('API_URL:', API_URL);
   const res = await fetch(`${API_URL}/api/tasks`);
 
   return res.json();
